3c0051cb713e3b61af41bd28bbf953b052427c11
Lucas Ruprecht <lruprecht@gmail.com>
2015-11-12 15:04:14 -0800

    checking in changes to routing and controllers on backend to have get/post methods against each social service

--- server/controllers/apiRoutes.js
+++ server/controllers/apiRoutes.js
@@ -4,7 +4,13 @@ var apiController = require('./apiController.js');
 module.exports = function (app) {
   // app === apiRouter injected from middlware.js
 
-  app.get('/fitbit', apiController.fitbit);
-  app.get('/twitter', apiController.twitter);
-  app.get('/github', apiController.github);
+  app.get('/fitbit', apiController.fitbit.get);
+  app.get('/twitter', apiController.twitter.get);
+  app.get('/github', apiController.github.get);
+  app.get('/facebook', apiController.facebook.get);
+  app.post('/fitbit', apiController.fitbit.post);
+  app.post('/twitter', apiController.twitter.post);
+  app.post('/github', apiController.github.post);
+  app.post('/facebook', apiController.facebook.post);
+
 };
