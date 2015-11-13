373cfd55240bd3da9dcbcb455641771a96d2fd5d
Lucas Ruprecht <lruprecht@gmail.com>
2015-11-12 21:15:33 -0800

    checking in initial work on FB auth (totally untested, not attempting to merge yet, just want to rebase)

--- server/config/middleware.js
+++ server/config/middleware.js
@@ -1,6 +1,12 @@
 // var morgan = require('morgan'); // used for logging incoming request
 var bodyParser = require('body-parser');
 var helpers = require('./helpers.js'); // our custom middleware
+var passport = require('passport');
+var FacebookStrategy = require('passport-facebook').Strategy;
+var cookieParser = require('cookie-parser');
+
+var FACEBOOK_APP_ID = "1631022490495363";
+var FACEBOOK_APP_SECRET = "94fb8b098c0b2ffcd7287f1a00dcd05a";
 
 var partials = require('express-partials');
 
@@ -17,6 +23,51 @@ module.exports = function (app, express) {
   app.use(bodyParser.urlencoded({extended: true}));
   app.use(bodyParser.json());
   app.use(express.static(__dirname + '/../../client'));
+  app.use(cookieParser());
+  app.use(session({ secret: 'keyboard cat' }));
+  // Initialize Passport!  Also use passport.session() middleware, to support
+  // persistent login sessions (recommended).
+  app.use(passport.initialize());
+  app.use(passport.session());
+
+  //////////////////////////////
+  //                          //
+  //  FACEBOOK AUTH PORTION   //
+  //                          //
+  //////////////////////////////
+
+  passport.use(new FacebookStrategy({
+    clientID: FACEBOOK_APP_ID,
+    clientSecret: FACEBOOK_APP_SECRET,
+    callbackURL: "http://localhost:3000/auth/facebook/callback"
+  },
+    function (accessToken, refreshToken, profile, done) {
+      // To keep the example simple, the user's Facebook profile is returned to
+      // represent the logged-in user.  In a typical application, you would want
+      // to associate the Facebook account with a user record in your database,
+      // and return that user instead.
+      return done(null, profile);
+    }
+  ));
+
+  app.get('/auth/facebook',
+    passport.authenticate('facebook'),
+    function (req, res) {
+      // The request will be redirected to Facebook for authentication, so this
+      // function will not be called.
+    });
+
+  app.get('/auth/facebook/callback',
+    passport.authenticate('facebook', { failureRedirect: '/signin' }),
+    function (req, res) {
+      res.redirect('/');
+    });
+
+  //////////////////////////////
+  //                          //
+  //   END FACEBOOK PORTION   //
+  //                          //
+  //////////////////////////////
 
 
   app.use('/users', userRouter); // use user router for all user request
@@ -31,3 +82,14 @@ module.exports = function (app, express) {
   require('../controllers/userRoutes.js')(userRouter);
   require('../controllers/apiRoutes.js')(apiRouter);
 };
+
+
+function ensureAuthenticated(req, res, next) {
+  if (req.isAuthenticated()) {
+    return next();
+  }
+  res.redirect('/signin');
+}
+
+
+
